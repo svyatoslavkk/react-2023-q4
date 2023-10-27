83d9f6cfc5b333a1bdfe786f0a980f0cf7f8f18e
svyatoslavkk <sv.krivonos1@gmail.com>
2023-10-27 14:40:29 +0300

    feat: add interfaces file

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,16 @@
+export interface Character {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  image: string;
+}
+
+export interface SearchComponentProps {
+  updateResults: (results: Character[]) => void;
+}
+
+export interface SearchComponentState {
+  searchTerm: string;
+  loading: boolean;
+}
