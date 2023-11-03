b36c80bde66bedc2b77d03a0cb293e2d7b78ae76
svyatoslavkk <sv.krivonos1@gmail.com>
2023-11-03 18:57:32 +0300

    add: Pagination component

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+}
+
+export default function Pagination({
+  currentPage,
+  totalPages,
+}: PaginationProps) {
+  const navigate = useNavigate();
+
+  const handlePageChange = (newPage: number) => {
+    navigate(`?page=${newPage}`);
+  };
+
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  return (
+    <ul className="pagination">
+      {pages.map((page) => (
+        <li key={page} className={page === currentPage ? 'active' : ''}>
+          <button onClick={() => handlePageChange(page)}>{page}</button>
+        </li>
+      ))}
+    </ul>
+  );
+}
