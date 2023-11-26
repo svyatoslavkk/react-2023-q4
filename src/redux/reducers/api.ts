79d3d02aea051e3b1e9eca02b8cc0dd559935822
svyatoslavkk <sv.krivonos1@gmail.com>
2023-11-26 14:26:24 +0300

    refactor: createapi function

--- src/redux/reducers/api.ts
+++ src/redux/reducers/api.ts
@@ -1,10 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { HYDRATE } from 'next-redux-wrapper';
 
 export const cardsApi = createApi({
   reducerPath: 'cardsApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://stapi.co/api/v1/rest/animal/search',
   }),
+  extractRehydrationInfo(action, { reducerPath }) {
+    if (action.type === HYDRATE) {
+      return action.payload[reducerPath];
+    }
+  },
   endpoints: (build) => ({
     searchByValue: build.mutation({
       query: (params) => ({
@@ -19,4 +25,9 @@ export const cardsApi = createApi({
   }),
 });
 
-export const { useSearchByValueMutation } = cardsApi;
+export const {
+  useSearchByValueMutation,
+  util: { getRunningQueriesThunk },
+} = cardsApi;
+
+export const { searchByValue } = cardsApi.endpoints;
