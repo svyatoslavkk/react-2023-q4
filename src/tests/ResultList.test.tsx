8520b03a568125193896f9235352f6256158d5f8
svyatoslavkk <sv.krivonos1@gmail.com>
2023-11-27 00:55:57 +0300

    refactor: tests for ssr

--- src/tests/ResultList.test.tsx
+++ src/tests/ResultList.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { describe, expect, it, vi } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import { createCardsListResponseMock } from './mocks/_mocks';
 import ResultList from '../components/ResultList';
 
@@ -8,18 +8,23 @@ vi.mock('next/router', () => vi.importActual('next-router-mock'));
 
 describe('Tests for the Card List component', () => {
   it('Component renders the specified number of cards - 7', async () => {
-    render(
-      <ResultList searchResults={createCardsListResponseMock(7, 10, false)} />,
-    );
+    await act(async () => {
+      render(
+        <ResultList
+          searchResults={createCardsListResponseMock(7, 10, false)}
+        />,
+      );
 
-    expect(await screen.findAllByText(/testCard/)).toHaveLength(7);
+      expect(await screen.findAllByText(/testCard/)).toHaveLength(7);
+    });
   });
 
   it('An appropriate message is displayed if no cards are present', async () => {
-    render(<ResultList searchResults={[]} />);
+    await act(async () => {
+      render(<ResultList searchResults={[]} />);
 
-    expect(screen.getByText('No results found.')).toBeTruthy();
-
-    expect(screen.queryByText(/testCard/)).toBeFalsy();
+      expect(screen.getByText('No results found.')).toBeTruthy();
+      expect(screen.queryByText(/testCard/)).toBeFalsy();
+    });
   });
 });
--- src/tests/Search.test.tsx
+++ src/tests/Search.test.tsx
@@ -1,49 +1,58 @@
 import React from 'react';
 import { describe, expect, it, vi } from 'vitest';
 import '@testing-library/jest-dom';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 import mockRouter from 'next-router-mock';
 import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider';
 import Search from '../components/Search';
 
 vi.mock('next/router', () => vi.importActual('next-router-mock'));
 
-describe('Tests for the Search component', () => {
-  it('Clicking the Search button saves the entered value to the local storage', async () => {
-    mockRouter.push('/');
-    render(<Search searchValue="" />, {
-      wrapper: MemoryRouterProvider,
+describe('Тесты для компонента Search', () => {
+  it('Нажатие на кнопку "Поиск" сохраняет введенное значение в локальное хранилище', async () => {
+    await act(async () => {
+      mockRouter.push('/');
+      render(<Search searchValue="" />, {
+        wrapper: MemoryRouterProvider,
+      });
+
+      expect(localStorage.getItem('search')).toBe(null);
+
+      fireEvent.change(screen.getByTestId('search-input'), {
+        target: { value: 'hotdog' },
+      });
+      fireEvent.click(screen.getByText('Search'));
+
+      expect(mockRouter.asPath).toBe('/?search=hotdog');
+      expect(localStorage.getItem('search')).toBe('hotdog');
+
+      fireEvent.change(screen.getByTestId('search-input'), {
+        target: { value: 'cat' },
+      });
+      fireEvent.click(screen.getByText('Search'));
+
+      expect(mockRouter.asPath).toBe('/?search=cat');
+      expect(localStorage.getItem('search')).toBe('cat');
     });
-    expect(localStorage.getItem('search')).toBe(null);
-
-    fireEvent.change(screen.getByTestId('search-input'), {
-      target: { value: 'hotdog' },
-    });
-    fireEvent.click(screen.getByText('Search'));
-
-    expect(mockRouter.asPath).toBe('/?search=hotdog');
-    expect(localStorage.getItem('search')).toBe('hotdog');
-
-    fireEvent.change(screen.getByTestId('search-input'), {
-      target: { value: 'cat' },
-    });
-    fireEvent.click(screen.getByText('Search'));
-
-    expect(mockRouter.asPath).toBe('/?search=cat');
-    expect(localStorage.getItem('search')).toBe('cat');
   });
 
-  it('The component retrieves the value from the local storage upon mounting.', async () => {
-    localStorage.setItem('search', 'cat');
-    const localStorageValue = localStorage.getItem('search');
-    expect(localStorageValue).toBe('cat');
+  it('Компонент извлекает значение из локального хранилища при монтировании.', async () => {
+    await act(async () => {
+      localStorage.setItem('search', 'cat');
+      const localStorageValue = localStorage.getItem('search');
 
-    mockRouter.push(`/?search=${localStorageValue}`);
-    render(<Search searchValue={mockRouter.query.search?.toString() || ''} />, {
-      wrapper: MemoryRouterProvider,
+      expect(localStorageValue).toBe('cat');
+
+      mockRouter.push(`/?search=${localStorageValue}`);
+      render(
+        <Search searchValue={mockRouter.query.search?.toString() || ''} />,
+        {
+          wrapper: MemoryRouterProvider,
+        },
+      );
+
+      expect(screen.getByTestId('search-form')).toMatchSnapshot();
+      expect(screen.getByTestId('search-input')).toHaveValue(localStorageValue);
     });
-
-    expect(screen.getByTestId('search-form')).toMatchSnapshot();
-    expect(screen.getByTestId('search-input')).toHaveValue(localStorageValue);
   });
 });
